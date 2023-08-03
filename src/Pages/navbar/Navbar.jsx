074f9d0a5d29a7989e1fd8cfb3eb4a56972ce74e
Ashik465 <helloashikul@gmail.com>
2023-08-04 00:31:36 +0600

    navbar done

--- src/Pages/navbar/Navbar.jsx
+++ src/Pages/navbar/Navbar.jsx
@@ -4,8 +4,13 @@ const Navbar = () => {
     return (
         <>
 
-<div className="navbar bg-neutral text-neutral-content">
-  <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+<div className="navbar bg-white text-neutral-content  border-b border-stone-300">
+  {/* <a className="btn btn-ghost normal-case text-xl">daisyUI</a> */}
+  <div className="flex gap-5  items-center justify-center">
+      <div className="w-6 h-6  -rotate-45 bg-gradient-to-br from-cyan-400 to-indigo-500 rounded" />
+  <div className="text-indigo-500 text-2xl font-bold leading-loose">Job Task</div>
+  </div>
+
 </div>
             
         </>
