40b6da9bb83a09e547da06416f8dd228b27ff032
Ashik465 <helloashikul@gmail.com>
2023-08-04 01:36:14 +0600

    from start

--- src/Pages/Login/LogIn.jsx
+++ src/Pages/Login/LogIn.jsx
@@ -2,8 +2,11 @@
 
 const LogIn = () => {
     return (
-        <>
-           <h1 className="text-red-500">this is log in</h1>
+        <>  <h1 className=" text-center mt-20 text-zinc-800 text-3xl md:text-5xl font-semibold leading-10">Welcome To Task Job</h1>
+           <div className="w-screen h-screen flex-col text-center justify-center items-center gap-20 inline-flex">
+            {/* from start */}
+  
+</div>
         </>
     );
 };
