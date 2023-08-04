4793c13ebb731a3f13ce924349056b8a22a15f8e
Ashik465 <helloashikul@gmail.com>
2023-08-04 22:45:04 +0600

    fire-base-auth-provider

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcGIbfaglm4wzKt-TIyBEjdqAln8jb5bc",
+  authDomain: "job-task-dashboard.firebaseapp.com",
+  projectId: "job-task-dashboard",
+  storageBucket: "job-task-dashboard.appspot.com",
+  messagingSenderId: "196553501418",
+  appId: "1:196553501418:web:254eb7fcf9912e85854593"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import router from './routes/MainRoutes'
+import AuthProvider from './provider/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <AuthProvider>
+    <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </AuthProvider>
+  
 )
