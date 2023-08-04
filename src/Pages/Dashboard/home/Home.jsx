37d3eb55e83b58d6fdc9319cbc8b43d97c8c834f
Ashik465 <helloashikul@gmail.com>
2023-08-05 02:37:04 +0600

    dashboard-home-half-done

--- src/Pages/Dashboard/home/Home.jsx
+++ src/Pages/Dashboard/home/Home.jsx
@@ -1,10 +1,119 @@
-
-
+import icon1 from '../../../assets/picture/Icon1.png'
+import icon2 from '../../../assets/picture/Icon2.png'
+import icon3 from '../../../assets/picture/Icon3.png'
+import icon4 from '../../../assets/picture/Icon4.png'
 const Home = () => {
     return (
-        <div>
-            this is home
+        <>
+            <div className="flex gap-5 p-10">
+
+                {/* 1st one */}
+
+                <div className="w-96 h-48 px-6 py-7 bg-white rounded-2xl flex-col justify-start items-start gap-2.5 inline-flex shadow-lg">
+  <div className="self-stretch grow shrink basis-0 flex-col justify-between items-start gap-6 flex">
+    <div className="justify-start items-center gap-4 inline-flex">
+      <div className="w-12 h-12 relative">
+        <div className="w-12 h-12 left-0 top-0 absolute bg-gradient-to-bl from-purple-500 to-blue-500 rounded-3xl" />
+        <div className="w-6 h-6 left-[12px] top-[12px] absolute">
+          <img className="w-4 h-4 left-[3px] top-[3px] absolute" src={icon1} />
         </div>
+      </div>
+      <div className="flex-col justify-start items-start gap-0.5 inline-flex">
+        <div className="text-sky-950 text-2xl font-semibold leading-9">55</div>
+        <div className="text-zinc-500 text-xs font-normal leading-none">Course completed</div>
+      </div>
+    </div>
+    <div className="self-stretch justify-start items-center gap-6 inline-flex">
+      <div className="grow shrink basis-0 h-1 relative">
+        <div className="w-64 h-1 left-0 top-0 absolute opacity-5 bg-sky-950 rounded-2xl" />
+        <div className="w-52 h-1 left-0 top-0 absolute bg-blue-500 rounded-2xl" />
+      </div>
+      <div className="text-right text-sky-950 text-sm font-medium leading-tight">70%</div>
+    </div>
+  </div>
+</div>
+{/* 2nd one */}
+
+<div className="w-96 h-48 px-6 py-7 bg-white rounded-2xl flex-col justify-start items-start gap-2.5 inline-flex shadow-lg">
+  <div className="self-stretch grow shrink basis-0 flex-col justify-between items-start gap-6 flex">
+    <div className="justify-start items-center gap-4 inline-flex">
+      <div className="w-12 h-12 relative">
+        <div className="w-12 h-12 left-0 top-0 absolute bg-gradient-to-bl from-orange-300 to-red-500 rounded-3xl" />
+        <div className="w-6 h-6 left-[12px] top-[12px] absolute">
+          <img className="w-3.5 h-5 left-[5px] top-[2px] absolute" src={icon2} />
+        </div>
+      </div>
+      <div className="flex-col justify-start items-start gap-0.5 inline-flex">
+        <div className="text-sky-950 text-2xl font-semibold leading-9">20</div>
+        <div className="text-zinc-500 text-xs font-normal leading-none">Certificate earned</div>
+      </div>
+    </div>
+    <div className="self-stretch justify-start items-center gap-5 inline-flex">
+      <div className="grow shrink basis-0 h-1 relative">
+        <div className="w-64 h-1 left-0 top-0 absolute opacity-5 bg-sky-950 rounded-2xl" />
+        <div className="w-20 h-1 left-0 top-0 absolute bg-red-400 rounded-2xl" />
+      </div>
+      <div className="text-right text-sky-950 text-sm font-medium leading-tight">20%</div>
+    </div>
+  </div>
+</div>
+
+{/* 3rd */}
+<div className="w-96 h-48 px-6 py-7 bg-white rounded-2xl flex-col justify-start items-start gap-2.5 inline-flex shadow-lg">
+  <div className="self-stretch grow shrink basis-0 flex-col justify-between items-start gap-6 flex">
+    <div className="justify-start items-center gap-4 inline-flex">
+      <div className="w-12 h-12 relative">
+        <div className="w-12 h-12 left-0 top-0 absolute bg-gradient-to-bl from-orange-400 to-yellow-400 rounded-3xl" />
+        <div className="w-6 h-6 left-[12px] top-[12px] absolute">
+          <img className="w-3.5 h-5 left-[5px] top-[2px] absolute" src={icon3} />
+        </div>
+      </div>
+      <div className="flex-col justify-start items-start gap-0.5 inline-flex">
+        <div className="text-sky-950 text-2xl font-semibold leading-9">25</div>
+        <div className="text-zinc-500 text-xs font-normal leading-none">Course in progress</div>
+      </div>
+    </div>
+    <div className="self-stretch justify-start items-center gap-5 inline-flex">
+      <div className="grow shrink basis-0 h-1 relative">
+        <div className="w-64 h-1 left-0 top-0 absolute opacity-5 bg-sky-950 rounded-2xl" />
+        <div className="w-28 h-1 left-0 top-0 absolute bg-amber-400 rounded-2xl" />
+      </div>
+      <div className="text-right text-sky-950 text-sm font-medium leading-tight">30%</div>
+    </div>
+  </div>
+</div>
+
+{/* 4rd */}
+
+<div className="w-96 h-48 px-6 py-7 bg-white rounded-2xl flex-col justify-start items-start gap-2.5 inline-flex shadow-lg">
+  <div className="self-stretch grow shrink basis-0 flex-col justify-between items-start gap-6 flex">
+    <div className="justify-start items-center gap-4 inline-flex">
+      <div className="w-12 h-12 relative">
+        <div className="w-12 h-12 left-0 top-0 absolute bg-gradient-to-b from-green-500 to-emerald-400 rounded-3xl" />
+        <div className="w-6 h-6 left-[12px] top-[12px] absolute">
+          <img className="w-4 h-4 left-[4px] top-[3px] absolute" src={icon4} />
+        </div>
+      </div>
+      <div className="flex-col justify-start items-start gap-0.5 inline-flex">
+        <div className="text-sky-950 text-2xl font-semibold leading-9">86</div>
+        <div className="text-zinc-500 text-xs font-normal leading-none">Total Course </div>
+      </div>
+    </div>
+    <div className="self-stretch justify-start items-center gap-4 inline-flex">
+      <div className="text-blue-500 text-sm font-medium leading-tight">View Courses</div>
+    </div>
+  </div>
+</div>
+
+
+
+            </div>
+
+            {/* rechart */}
+            <div>
+                
+            </div>
+        </>
     );
 };
 
